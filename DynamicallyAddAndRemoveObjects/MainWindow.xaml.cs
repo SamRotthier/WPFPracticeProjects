657fd84c81b679a7d109c729017e7a8d174b790f
samro <sam.rotthier@student.kdg.be>
2025-01-20 22:08:30 +0100

    Adding and removing a square (in code behinde)

--- DynamicallyAddAndRemoveObjects/MainWindow.xaml.cs
+++ DynamicallyAddAndRemoveObjects/MainWindow.xaml.cs
@@ -16,9 +16,42 @@ namespace DynamicallyAddAndRemoveObjects
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Brush customColor;
+        Random r = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddOrRemoveItems(object sender, MouseButtonEventArgs e)
+        {
+            if(e.OriginalSource is Rectangle)
+            {
+                Rectangle activeRectangle = (Rectangle)e.OriginalSource;
+
+                myCanvas.Children.Remove(activeRectangle);
+            }
+            else
+            {
+                customColor = new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 255)));
+
+                Rectangle newRextangle = new Rectangle
+                {
+                    Width = 50,
+                    Height = 50,
+                    Fill = customColor,
+                    StrokeThickness = 3,
+                    Stroke = Brushes.Black
+                };
+                
+                Canvas.SetLeft(newRextangle, Mouse.GetPosition(myCanvas).X);
+                Canvas.SetTop(newRextangle, Mouse.GetPosition(myCanvas).Y);
+
+                myCanvas.Children.Add(newRextangle);
+            }
+
         }
     }
 }
